c60406e4a12ca965ecb63fde0c2a74328091fb9d
Aryan rana <aryan.rana@adypu.edu.in>
2025-03-30 03:10:09 +0530

    30 march 2025

---
+++ dsa1.py
@@ -0,0 +1,71 @@
+# class Student:
+#     school = "Newton"
+
+#     def __init__(self,val):
+#         self.name = val
+#     def addsurname(self,val):
+#         self.surname = val
+#     def fullname(self,val):
+#         self.fullname = val
+
+# s1=Student("Adityta")
+# print(s1.name)
+
+
+# s2 = Student("Nishi")
+# print(s2.name)
+
+# s2.addsurname("Gupta")
+# print(s2.surname)
+
+# s2.fullname("Nishi Gupta")
+# print(s2.fullname)
+
+# print(s1.school)
+# print(s2.school)
+# print(Student.school)
+# print(Student.name)
+
+# class fractional:
+#     def __init__(self,numerator,denominator):
+#         self.numerator=numerator
+#         self.denominator=denominator
+#     def print(self):
+#         print(self.numerator,"/",self.denominator)
+# s1=fractional(2,5)
+# s1.print()
+# s2=fractional(3,4)
+# s2.print()
+# fibonacci series
+# def fibonacci (n):
+#     if n==1:
+#         return 1
+#     elif n==0:
+#         return 0
+#     else :
+#         return fibonacci(n-1)+fibonacci(n-2)
+# print(fibonacci(14))
+#sets
+# s={3,4,5,3}
+# t={"hii",23,4,34.55}
+# print(t)
+# f=set()
+# print(type (f))
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         s=0
+#         d=[]
+#         for i in nums:
+#             if i == s:
+#                 print(d)
+#             else:
+#                 s+=i
+#                 d.append(i)
+print(11)
+print(11,end=" ")
+print(11)
