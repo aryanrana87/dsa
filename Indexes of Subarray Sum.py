a501ee4aaacf3030d06be02dbd8957cc6b31bbe2
Aryan rana <aryan.rana@adypu.edu.in>
2025-04-27 15:44:29 +0530

    solution

--- Indexes of Subarray Sum.py
+++ Indexes of Subarray Sum.py
@@ -1,7 +1,6 @@
 # Given an array arr[] containing only non-negative integers, your task is to find a continuous subarray (a contiguous sequence of elements) whose sum equals a specified value target. 
 # You need to return the 1-based indices of the leftmost and rightmost elements of this subarray. You need to find the first subarray whose sum is equal to the target.
 
-
 arr= [1, 2, 3, 7, 5]
 target = 12
 n = len(arr)
